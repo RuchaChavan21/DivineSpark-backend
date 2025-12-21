6f4a333e3c36fd526ba8425c3b65f83f2aa6a5dd
rucha <ruchachavan21@gmail.com>
2025-12-21 19:20:33 +0530

    Initial commit

---
+++ divinespark-backend/src/main/java/com/divinespark/DivinesparkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.divinespark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivinesparkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivinesparkBackendApplication.class, args);
+	}
+
+}
---
+++ divinespark-backend/src/test/java/com/divinespark/DivinesparkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divinespark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivinesparkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
