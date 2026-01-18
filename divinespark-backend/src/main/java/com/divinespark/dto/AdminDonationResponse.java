7e2cabe2cb5b2824f9179305d2e1f2681dabfce3
Neha <neha.narkhede2005@gmail.com>
2026-01-18 19:03:50 +0530

    Final fixes

--- divinespark-backend/src/main/java/com/divinespark/dto/AdminDonationResponse.java
+++ divinespark-backend/src/main/java/com/divinespark/dto/AdminDonationResponse.java
@@ -6,6 +6,9 @@ public class AdminDonationResponse {
 
     private Long id;
     private Long userId;
+    private String orderId;
+    private String userName;     // NEW
+    private String contactNumber;
     private double amount;
     private String note;
     private String status;
@@ -23,4 +26,28 @@ public class AdminDonationResponse {
     public void setStatus(String status) { this.status = status; }
     public LocalDateTime getCreatedAt() { return createdAt; }
     public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
